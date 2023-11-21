5bce66e40d1eeac21485293873943ee7ff571ecb
ruben <rubenvdb-work@outlook.com>
2023-11-21 15:24:18 +0100

    second code cleanup

--- src/Display.cpp
+++ src/Display.cpp
@@ -12,34 +12,22 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 Display::Display()
 {
-    //use spi to start communicating with display
+    // use spi to start communicating with display
     tft.begin();
 
-    //set horizontal position
+    // set horizontal position
     tft.setRotation(1);
 
-    //color full screen black
+    // color full screen black
     tft.fillScreen(ILI9341_BLACK);
 }
 
-unsigned long Display::draw_circle(uint16_t x, uint8_t y, uint8_t circle_radius){
-    // start time of function is stored
-    unsigned long start = micros();
-
-    // center of display in position 1: x = 170, y = 120
+void Display::draw_circle(uint16_t x, uint8_t y, uint8_t circle_radius)
+{
     tft.fillCircle(x, y, circle_radius, ILI9341_WHITE);
-
-   // return how long the function took to complete
-    return micros() - start;//todo
 }
 
-unsigned long Display::clear_circle(uint16_t x, uint8_t y, uint8_t circle_radius){
-    // start time of function is stored
-    unsigned long start = micros();
-
-    // center of display in position 1: x = 170, y = 120
+void Display::clear_circle(uint16_t x, uint8_t y, uint8_t circle_radius)
+{
     tft.fillCircle(x, y, circle_radius, ILI9341_BLACK);
-
-   // return how long the function took to complete
-    return micros() - start;
 }
