cfbeb49f13a1e766ee48cc108ef72af2a796a373
JasperV <jasper@vanveenhuizen.nl>
2023-11-29 01:44:22 +0100

    comments

--- src/Joystick.h
+++ src/Joystick.h
@@ -11,27 +11,19 @@ public:
     /// @brief Update joystick data with new states.
     /// This updates ALL states, including joystick & buttons.
     /// @return true if succeeded, false if failed to read memory
-    bool store_state(void);
+    bool store_state();
 
-    /// @brief Get the X-axis from the joystick
-    /// Update states with store_state() first to get new data.
-    /// @return Y-axis from current state
-    uint8_t get_x_axis(void);
+    /// @return [3...252] where 3=left, 128=centre, 252=right
+    uint8_t get_x_axis();
 
-    /// @brief Get the Y-axis from the joystick
-    /// Update states with store_state() first to get new data.
-    /// @return Y-axis from current state
-    uint8_t get_y_axis(void);
+    /// @return [2...253] where 2=bottom, 128=centre, 253=top
+    uint8_t get_y_axis();
 
-    /// @brief Get the state of the C button
-    /// Update states with store_state() first to get new data.
     /// @return if C button is pressed
-    bool is_c_pressed(void);
+    bool is_c_pressed();
 
-    /// @brief Get the state of the Z button
-    /// Update states with store_state() first to get new data.
     /// @return if Z button is pressed
-    bool is_z_pressed(void);
+    bool is_z_pressed();
 };
 
 #endif
