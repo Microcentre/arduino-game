86cb83c764a85154f641d8b400d82792b4527185
Ruben <s1185125@student.windesheim.nl>
2023-12-08 17:10:38 +0100

    name change

--- src/ShowHealthOnSSD.cpp
+++ src/ShowHealthOnSSD.cpp
@@ -7,7 +7,7 @@ ShowHealthOnSSD::ShowHealthOnSSD(Player *Player)
 
 void ShowHealthOnSSD::update(Player *Player)
 {
-    Wire.beginTransmission(0x21); //start transmission to slave (I/O expander)
-    Wire.write(display[Player->health]); //send digit to display
-    Wire.endTransmission(); //close connection
+    Wire.beginTransmission(0x21);               // start transmission to slave (I/O expander)
+    Wire.write(DISPLAY_DIGITS[Player->health]); // send digit to display
+    Wire.endTransmission();                     // close connection
 }
--- src/ShowHealthOnSSD.h
+++ src/ShowHealthOnSSD.h
@@ -8,23 +8,22 @@ class ShowHealthOnSSD : public Player::HurtObserver
 {
 public:
     /// @brief executes update once to start showing health when player is created
-    /// @param player 
+    /// @param player
     ShowHealthOnSSD(Player *player);
 
     /// @brief updates SSD to display current health amount
-    /// @param player 
+    /// @param player
     void update(Player *player) override;
 
 private:
     /// @brief contains binary values to display numbers on SSD
-    const uint8_t display[4] = {
-        //0 = led on, 1 = led off (anode sevenseg display)
+    const uint8_t DISPLAY_DIGITS[4] = {
+        // 0 = led on, 1 = led off (anode sevenseg display)
         0b11000000, // 0
         0b11111001, // 1
         0b10100100, // 2
         0b10110000, // 3
     };
-
-    };
+};
 
 #endif
