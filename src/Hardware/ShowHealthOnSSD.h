95709396e6796671dfcc37578bb851e668c94ff1
ruben <rubenvdb-work@outlook.com>
2023-12-19 13:29:09 +0100

    fixed bug

--- src/Hardware/ShowHealthOnSSD.h
+++ src/Hardware/ShowHealthOnSSD.h
@@ -11,19 +11,24 @@ public:
     /// @param player
     ShowHealthOnSSD(Player *player);
 
+    ~ShowHealthOnSSD();
+
     /// @brief updates SSD to display current health amount
     /// @param player
     void update(Player *player) override;
 
 private:
     /// @brief contains binary values to display numbers on SSD
-    const uint8_t DISPLAY_DIGITS[4] = {
+    const uint8_t DISPLAY_DIGITS[5] = {
         // 0 = led on, 1 = led off (anode sevenseg display)
         0b11000000, // 0
         0b11111001, // 1
         0b10100100, // 2
         0b10110000, // 3
+        0b11111111, // off
     };
+
+    const uint8_t DISPLAY_OFF = 4;
 };
 
 #endif
