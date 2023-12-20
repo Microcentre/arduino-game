62b25ed6edf23870fac76b08a52dfece8611d345
JasperV <jasper@vanveenhuizen.nl>
2023-12-20 13:57:16 +0100

    fix incorrect player_facing_direction

--- src/Hardware/IR/GameData.h
+++ src/Hardware/IR/GameData.h
@@ -8,7 +8,7 @@ class GameData
 public:
     uint16_t player_x_position;
     uint8_t player_y_position;
-    uint16_t player_facing_direction;
+    double player_facing_direction;
     bool player_shot_bullet;
     bool player_died;
     bool wave_ended;
